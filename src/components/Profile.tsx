29c5922d007d18628ac8b02b3d9a66886b081664
Hp <ahsanali.official085@gmail.com>
2024-11-09 11:41:54 +0500

    Added history into profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,12 @@
-import {StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  FlatList,
+  ScrollView,
+} from 'react-native';
 import React, {FunctionComponent, useState, useEffect} from 'react';
 import {
   responsiveFontSize,
@@ -19,6 +27,27 @@ const Profile: FunctionComponent = ({navigation}) => {
   const [loader, setLoader] = useState(false);
   const userData = useSelector(state => state.userReducer.userData);
   const dispatch = useDispatch();
+  const [data, setData] = useState();
+
+  const fetchUserHistory = async () => {
+    setLoader(true);
+    const HistoryData = {
+      _id: userData._id,
+    };
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/api/v1/users/watch-history`,
+        HistoryData,
+      );
+      const result = res.data;
+      console.log('res.data', res.data.data[0]);
+      setData(result?.data[0]?.watchHistory);
+      setLoader(false);
+    } catch (error) {
+      setLoader(false);
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     navigation.addListener('focus', () => {
@@ -26,24 +55,28 @@ const Profile: FunctionComponent = ({navigation}) => {
         tabBarStyle: [
           {
             position: 'absolute',
-            bottom: 4,
-            // left: 20,
-            // right: 20,
-            elevation: 0,
-            backgroundColor: '#fff',
-            height: 60,
-            borderRadius: 10,
+              bottom: 4,
+              // left: 20,
+              // right: 20,
+              elevation: 0,
+              backgroundColor: '#fff',
+              height: 60,
+              borderRadius: 10,
             ...styles.shadow,
           },
         ],
       });
     });
+    fetchUserHistory();
   }, []);
 
   const logOutUser = () => {
     setLoader(true);
+    console.log(' userData._id', userData._id);
     axios
-      .post(`${BASE_URL}/api/v1/users/logout`, {_id: userData._id})
+      .post(`${BASE_URL}/api/v1/users/logout`, {
+        _id: '6704100747caed00633fdecc',
+      })
       .then(res => {
         const result = res.data;
         setLoader(false);
@@ -56,9 +89,42 @@ const Profile: FunctionComponent = ({navigation}) => {
       });
   };
 
+  const convertVideoTime = time => {
+    const durationInSeconds = Math.floor(time);
+    const minutes = Math.floor(durationInSeconds / 60); // Get the whole minutes
+    const seconds = durationInSeconds % 60;
+    return `${minutes}:${seconds}`;
+  };
+
+  function timeAgo(date) {
+    const now = new Date();
+    const past = new Date(date);
+    const diffInSeconds = Math.floor((now - past) / 1000);
+
+    const timeIntervals = {
+      year: 31536000, // 60 * 60 * 24 * 365
+      month: 2592000, // 60 * 60 * 24 * 30
+      week: 604800, // 60 * 60 * 24 * 7
+      day: 86400, // 60 * 60 * 24
+      hour: 3600, // 60 * 60
+      minute: 60,
+      second: 1,
+    };
+
+    for (const [unit, secondsInUnit] of Object.entries(timeIntervals)) {
+      const elapsed = Math.floor(diffInSeconds / secondsInUnit);
+
+      if (elapsed >= 1) {
+        return `${elapsed} ${unit}${elapsed > 1 ? 's' : ''} ago`;
+      }
+    }
+
+    return 'just now';
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.subContainer}>
+      <ScrollView style={styles.subContainer}>
         <Loader showModal={loader} LoaderColor={'black'} LoaderSize={'large'} />
         <TouchableOpacity
           onPress={() =>
@@ -77,10 +143,101 @@ const Profile: FunctionComponent = ({navigation}) => {
           </View>
         </TouchableOpacity>
         <View style={styles.infoView}>
-          <TouchableOpacity style={styles.infoOpacity}>
-            <FontAwesome name="history" color="#000" style={styles.infoIcon} />
-            <Text style={styles.infoText}>History</Text>
-          </TouchableOpacity>
+          <View style={styles.infoOpacity1}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <View style={{flexDirection: 'row'}}>
+                <FontAwesome
+                  name="history"
+                  color="#000"
+                  style={styles.infoIcon}
+                />
+                <Text style={styles.infoText}>History</Text>
+              </View>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('History')}
+                style={{
+                  borderColor: 'black',
+                  borderWidth: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  padding: 4,
+                  borderRadius: responsiveWidth(5),
+                }}>
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: responsiveFontSize(1.8),
+                    fontWeight: '400',
+                  }}>
+                  View all
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.subView}>
+              <FlatList
+                data={data}
+                horizontal
+                contentContainerStyle={{
+                  // paddingBottom: responsiveHeight(10),
+                }}
+                keyExtractor={item => item._id}
+                renderItem={({item}) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate('Watch', {
+                        item: {
+                          title: item?.title,
+                          channelName: item?.channelName,
+                          Views: item?.Views,
+                          uploadedDate: item?.createdAt,
+                          imageUrl: item?.imageUrl,
+                          duration: item?.duration,
+                          description: item?.description,
+                          videoLink: item?.videoFile,
+                          owner: item?.owner,
+                          avatar: item?.owner?.avatar,
+                          username: item?.owner?.username,
+                          videoId: item?._id,
+                        },
+                      });
+
+                      navigation.getParent()?.setOptions({
+                        tabBarStyle: {display: 'none'},
+                      });
+                    }}
+                    style={styles.flatListView}>
+                    <View style={styles.imageView}>
+                      <Image
+                        source={{uri: item.thumbnail}}
+                        style={styles.image}
+                      />
+                      <ScrollView horizontal style={styles.durationMain}>
+                        <View style={styles.duration}>
+                          <Text style={styles.durationText}>
+                            {convertVideoTime(item.duration)}
+                          </Text>
+                        </View>
+                      </ScrollView>
+                    </View>
+                    <View style={styles.videoTitle}>
+                      <Text style={styles.videTitleText}>
+                        {item?.title?.length > 40
+                          ? `${item.title.slice(0, 40)}....`
+                          : item.title}
+                      </Text>
+                      <Text style={styles.nameText}>
+                        {item?.owner?.username}
+                      </Text>
+                      <Text style={styles.videoViews}>{`${
+                        item.Views
+                      } views • ${timeAgo(item.createdAt)}`}</Text>
+                    </View>
+                  </TouchableOpacity>
+                )}
+              />
+            </View>
+          </View>
           <TouchableOpacity style={styles.infoOpacity}>
             <MaterialIcons
               name="video-library"
@@ -115,7 +272,7 @@ const Profile: FunctionComponent = ({navigation}) => {
             <Text style={styles.infoText}>Logout</Text>
           </TouchableOpacity>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -130,6 +287,7 @@ const styles = StyleSheet.create({
   subContainer: {
     marginTop: responsiveHeight(4),
     marginHorizontal: responsiveWidth(6),
+    marginBottom:responsiveHeight(8)
   },
   icon: {
     height: responsiveHeight(10),
@@ -160,7 +318,7 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   infoView: {
-    marginTop: responsiveHeight(4),
+    marginTop: responsiveHeight(1),
   },
   infoText: {
     color: '#000',
@@ -170,14 +328,106 @@ const styles = StyleSheet.create({
   infoOpacity: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginTop: responsiveHeight(3),
+    backgroundColor: '#fff',
+    elevation: 2,
+    marginBottom:responsiveHeight(1),
+    paddingVertical: responsiveWidth(5),
+    paddingHorizontal: responsiveWidth(2),
+    borderRadius: responsiveHeight(1),
+  },
+  infoOpacity1: {
+    // flexDirection: 'row',
+    // alignItems: 'center',
     marginTop: responsiveHeight(4),
     backgroundColor: '#fff',
     elevation: 2,
-    padding: responsiveWidth(5),
+    paddingVertical: responsiveWidth(5),
+    paddingHorizontal: responsiveWidth(2),
     borderRadius: responsiveHeight(1),
   },
   infoIcon: {
-    fontSize: responsiveFontSize(2.5),
+    fontSize: responsiveFontSize(2.4),
+    alignSelf: 'center',
     marginRight: responsiveWidth(6),
+  },
+  subView: {
+    marginTop: responsiveHeight(2),
+  },
+  button: {
+    height: responsiveHeight(7),
+    backgroundColor: '#000',
+    borderRadius: responsiveHeight(4),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: responsiveFontSize(2),
+    fontWeight: '600',
+  },
+  heading: {
+    fontSize: responsiveFontSize(3.3),
+    color: '#000',
+    marginTop: responsiveHeight(2),
+    fontWeight: 'bold',
+  },
+  flatListView: {
+    flex: 1,
+    marginHorizontal: responsiveWidth(2),
+  },
+  nameText: {
+    fontSize: responsiveFontSize(1.6),
+    marginTop: responsiveHeight(0.5),
+    color: 'gray',
+    flexWrap: 'wrap',
+  },
+  image: {
+    height: responsiveHeight(14),
+    width: responsiveWidth(45),
+    borderRadius: responsiveWidth(1),
+  },
+  duration: {
+    backgroundColor: '#000',
+    padding: responsiveHeight(0.5),
+    borderRadius: responsiveHeight(1),
+  },
+  durationText: {
+    color: '#fff',
+  },
+  durationMain: {
+    bottom: responsiveHeight(1),
+    right: responsiveWidth(2),
+    position: 'absolute',
+  },
+  imageView: {
+    height: responsiveHeight(14),
+    width: responsiveWidth(45),
+  },
+  videoTitle: {
+    marginLeft: responsiveWidth(2),
+    flex: 1,
+  },
+  videTitleText: {
+    fontSize: responsiveFontSize(2),
+    fontWeight: '500',
+    flexWrap: 'wrap',
+    color: '#000',
+  },
+  videoViews: {
+    fontSize: responsiveFontSize(1.6),
+    marginTop: responsiveHeight(1),
+    color: 'gray',
+    flexWrap: 'wrap',
+  },
+  shadow: {
+    shadowColor: '#FF2400',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
   },
 });
